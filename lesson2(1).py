d63643f46527d9d03a744fd78cecc95b4b4eb6a5
kostyan4rub <kostyan422rus@gmail.com>
2024-08-21 21:24:40 +0700

    Add files via upload

---
+++ lesson2(1).py
@@ -0,0 +1,5 @@
+example_one = "Количестов выполненых ДЗ:12"
+example_two = "Количество затраченных часов:1.5"
+name = "Python"
+example_three = "Время на одно задание: "
+print(name, example_one, example_two, example_three, 1.5/12)
