21550220f2da58e3a2811af05d9292e9bd6c1261
kostyan4rub <kostyan422rus@gmail.com>
2024-08-24 11:43:35 +0700

    Add files via upload

--- lesson2.py
+++ lesson2.py
@@ -1,5 +1,7 @@
-example_one = "Количестов выполненых ДЗ:12"
-example_two = "Количество затраченных часов:1.5"
+example_one = "Количестов выполненых ДЗ: "
+a = 12
+example_two = "Количество затраченных часов: "
+b = 1.5
 name = "Python"
-example_three = "Время на одно задание(example_one/example_two)"
-print(name, example_one, example_two, example_three)
+example_three = "среднее время выполнения:  "
+print(name, example_one, a, example_two, b, example_three, b/a)
