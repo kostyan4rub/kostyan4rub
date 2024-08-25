02159b6cb1b0c277602dfdb9a03b4a3b944d4cab
kostyan4rub <kostyan422rus@gmail.com>
2024-08-25 15:16:52 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Alisa': 2016, 'Anton' : 2019}
+print(my_dict['Anton'])
+my_dict['Natasha'] = 1989
+my_dict.update({'Konstantin' : 1988,
+                'Maxim' : 1986})
+del my_dict['Maxim']
+print(my_dict)
+my_set = {1, 2, 3, 3, 1, 2, 'sun', 'sky', 'sea', 'sky','sea', 'sun'}
+print(my_set)
+my_set.add(5)
+my_set.add(7)
+my_set.remove(3)
+print(my_set)
