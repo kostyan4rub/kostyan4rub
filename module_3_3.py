13460e28a23a4984e7ca262c739edc46c469fb90
kostyan4rub <kostyan422rus@gmail.com>
2024-09-11 21:39:48 +0700

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+print()
+values_list = ('Kostyan4', 123, True)
+print_params(*values_list)
+values_dict = {'a': 'text', 'b': False, 'c': 33}
+print_params(**values_dict)
+print()
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
