875789479210bc64682214054b864b3a81f8d8c9
kostyan4rub <kostyan422rus@gmail.com>
2024-12-21 11:11:25 +0700

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,15 @@
+# import requests
+# from PIL import Image
+# import numpy as np
+#
+# from requests import get
+# r = get('https://google.com/')
+# print(r.status_code)
+#
+#
+# image = Image.open('lake.jpg')
+# image.show()
+#
+#
+# b = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(b)
